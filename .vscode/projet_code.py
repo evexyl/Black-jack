eba1debb61f09d6b8039eda7583dacd2bafa37c7
Dmoniaque <lymariem2006@gmail.com>
2025-03-14 21:57:48 +0100

    win for real

--- .vscode/projet_code.py
+++ .vscode/projet_code.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import PhotoImage
 import os
-import pyglet 
 
 
 #FENETRE 1:
@@ -13,9 +12,6 @@ fenetre1.geometry("650x800")
 
 
 #Label
-bj = tk.Label(fenetre1, text="Black Jack", bg = "#006400", fg = "white", font = ("Impact", 54))
-bj.place(x=300,y=165)
-
 current_dir = os.path.dirname(__file__)
 image_path = os.path.join(current_dir, "casino.png")
 image = PhotoImage(file=image_path)
@@ -24,23 +20,25 @@ image_bj = tk.Label(fenetre1, image=image)
 image_bj.image = image
 image_bj.pack()
 
+bj = tk.Label(fenetre1, text="Black Jack",bg = "#006400", fg = "white", font=("Impact",54))
+bj.place(x=150,y=165)
 
 #Bouttons
 start = tk.Button(fenetre1, text = " START ", bg = '#42529c', fg = 'white')
 start.configure(height=5, width=30)
-start.place(x=200,y=400)
+start.place(x=200,y=300)
 
 regles = tk.Button(fenetre1,text = " REGLES ",bg = '#42529c',fg = 'white')
 regles.configure(height=5, width=30)
-regles.place(x=200,y=500)
+regles.place(x=200,y=400)
 
 hist = tk.Button(fenetre1,text = " HISTORIQUE ",bg = '#42529c',fg = 'white')
 hist.configure(height=5, width=30)
-hist.place(x=200,y=600)
+hist.place(x=200,y=500)
 
 bouton_quit = tk.Button(fenetre1,text = " QUIT ",bg = 'indian red',command = fenetre1.destroy)
 bouton_quit.configure(height=5, width=30)
-bouton_quit.place(x=200,y=700)
+bouton_quit.place(x=200,y=600)
 
 #mets fenetre1
 fenetre1.mainloop()
