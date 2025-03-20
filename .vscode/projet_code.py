ed738e56ea929e68844616c42a69c558207488a1
Flavie Albitre <albitre.f@gmail.com>
2025-03-20 13:55:37 +0100

    jsp ce qui ce passe

--- .vscode/projet_code.py
+++ .vscode/projet_code.py
@@ -20,27 +20,26 @@ image_bj = tk.Label(fenetre1, image=image)
 image_bj.image = image
 image_bj.pack()
 
-bj = tk.Label(fenetre1, text="Black Jack",bg = "#006400", fg = "white", font=("Impact",54))   #fond de black jack en noir et font=Helvetica
+bj = tk.Label(fenetre1, text="Black Jack",bg = "#006400", fg = "white", font=("Impact",54))
 bj.place(x=150,y=165)
 
-#Bouttons    #changer couleur bouton 
+#Bouttons
 start = tk.Button(fenetre1, text = " START ", bg = '#42529c', fg = 'white')
 start.configure(height=5, width=30)
-start.place(x=200,y=350)
+start.place(x=200,y=300)
 
 regles = tk.Button(fenetre1,text = " REGLES ",bg = '#42529c',fg = 'white')
 regles.configure(height=5, width=30)
-regles.place(x=200,y=450)
+regles.place(x=200,y=400)
+
+hist = tk.Button(fenetre1,text = " HISTORIQUE ",bg = '#42529c',fg = 'white')#A enlever
+hist.configure(height=5, width=30)
+hist.place(x=200,y=500)
 
 bouton_quit = tk.Button(fenetre1,text = " QUIT ",bg = 'indian red',command = fenetre1.destroy)
 bouton_quit.configure(height=5, width=30)
-bouton_quit.place(x=200,y=550)
+bouton_quit.place(x=200,y=600)
 
 #mets fenetre1
 fenetre1.mainloop()
 
-###############################################################################################
-#FENETRE 2
-
-###############################################################################################
-#FENETRE 3
