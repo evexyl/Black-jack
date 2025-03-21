68c1dc518fd73854e82d7f839634defc35bff695
ApolLo111306 <apolline.longin@ens.uvsq.fr>
2025-03-21 10:34:17 +0100

    couleurs fenetre 2

--- .vscode/projet_code.py
+++ .vscode/projet_code.py
@@ -37,7 +37,7 @@ def on_open_fenetre2():
     fenetre2 = tk.Toplevel()
     fenetre2.title("Black Jack JEU")
     fenetre2.geometry("1200x700")
-    fenetre2.config(bg = '#007a33')
+    fenetre2.config(bg = '#164e0a')
 
     def distribution():
         #utiliser random
@@ -53,24 +53,24 @@ def on_open_fenetre2():
         pass
 
     #LABELS
-    dealer = tk.Label(fenetre2, text ="Dealer", bg = '#007a33', fg = "black",)
+    dealer = tk.Label(fenetre2, text ="Croupier", bg = '#164e0a', fg = "white",)
     dealer.place(x=150,y=165)
 
-    joeur = tk.Label(fenetre2, text ="Joeur", bg = '#007a33', fg = "black",)
-    joeur.place(x=350,y=165)
+    joueur = tk.Label(fenetre2, text ="Joueur", bg = '#164e0a', fg = "white",)
+    joueur.place(x=350,y=165)
 
-    cmpt_d = tk.Label(fenetre2, text ="N", bg = '#007a33', fg = "black",)
+    cmpt_d = tk.Label(fenetre2, text ="N", bg = '#164e0a', fg = "white",)
     cmpt_d.place(x=250,y=165)
 
-    cmpt_j = tk.Label(fenetre2, text ="N", bg = '#007a33', fg = "black",)
+    cmpt_j = tk.Label(fenetre2, text ="N", bg = '#164e0a', fg = "white",)
     cmpt_j.place(x=450,y=165)
 
     #BOUTONS
-    deal = tk.Button(fenetre2, text = " DEAL ", bg = '#6cc24a', fg = 'white')
+    deal = tk.Button(fenetre2, text = " CARTE ! ", bg = '#7c0a0a', fg = 'white')
     deal.configure(height=3, width=10)
     deal.place(x=200,y=300)
 
-    stand = tk.Button(fenetre2, text = " STAND ", bg = '#6cc24a', fg = 'white')
+    stand = tk.Button(fenetre2, text = " RESTER ", bg = '#7c0a0a', fg = 'white')
     stand.configure(height=3, width=10)
     stand.place(x=300,y=300)
 
