2489641651928e8490d68a9e6faad4265d87ed69
Dmoniaque <lymariem2006@gmail.com>
2025-03-21 09:54:11 +0100

    bouton regle

--- .vscode/projet_code.py
+++ .vscode/projet_code.py
@@ -2,6 +2,36 @@ import tkinter as tk
 from tkinter import PhotoImage
 import os
 
+#Fenetre règle:
+#config de fenetre + canvas
+#callback fenetre regle
+def on_open_fenetre():
+    fentre_regle= tk.Toplevel()
+    fentre_regle.title("Black Jack REGLE")
+    fentre_regle.config(bg="black")
+    fentre_regle.geometry("650x800")
+
+
+
+#Label
+    rule=tk.Label(fentre_regle,fg="green", font=('Arial',14),wraplength=600,
+            text="But du jeu : Atteindre un total de points le plus proche de 21 sans dépasser "
+              "ce nombre.\n"
+              
+              "Valeurs des cartes : les cartes de 2 à 10 valent leur valeur.Les "
+              "figures (Roi, Dame, Valet) valent 10 points. L'As vaut 1 ou 11 points (selon "
+              "ce qui vous avantage).\n "
+
+              "Déroulement :Chaque joueur reçoit 2 cartes face visible, "
+              "et le croupier a une carte visible et une face cachée. Vous pouvez demander "
+              "d’autres cartes (tirer), ou garder votre total actuel (rester). Si vos cartes"
+              " dépassent 21, vous perdez (on appelle cela 'sauter'). \n "
+
+              "Gagner : Vous gagnez si votre total est égal à 21 ou le plus proche possible sans dépasser cette valeur. "
+              "Un Blackjack (21 avec deux cartes seulement, As + 10 ou figure) est la meilleure main possible.")  #\n permet le retour a la ligne
+    rule.pack()
+
+##############################################################################################################################
 
 #FENETRE 1:
 #config de fenetre + canvas
@@ -41,37 +71,6 @@ fenetre1.mainloop()
 
 ########################################################################################################################
 #FENETRE 2
-
-########################################################################################################################
-#Fenetre règle:
-
-#config de fenetre + canvas
-#callback fenetre regle
-def on_open_fenetre():
-    fentre_regle= tk.Toplevel()
-    fentre_regle.title("Black Jack REGLE")
-    fentre_regle.config(bg="black")
-    fentre_regle.geometry("650x800")
-
-
-
-#Label
-rule=tk.Label(fentre_regle,fg="green", font=('Arial',14),wraplength=600,
-              text="But du jeu : Atteindre un total de points le plus proche de 21 sans dépasser "
-              "ce nombre.\n"
-              
-              "Valeurs des cartes : les cartes de 2 à 10 valent leur valeur.Les "
-              "figures (Roi, Dame, Valet) valent 10 points. L'As vaut 1 ou 11 points (selon "
-              "ce qui vous avantage).\n "
-
-              "Déroulement :Chaque joueur reçoit 2 cartes face visible, "
-              "et le croupier a une carte visible et une face cachée. Vous pouvez demander "
-              "d’autres cartes (tirer), ou garder votre total actuel (rester). Si vos cartes"
-              " dépassent 21, vous perdez (on appelle cela 'sauter'). \n "
-
-              "Gagner : Vous gagnez si votre total est égal à 21 ou le plus proche possible sans dépasser cette valeur. "
-              "Un Blackjack (21 avec deux cartes seulement, As + 10 ou figure) est la meilleure main possible.")  #\n permet le retour a la ligne
-rule.pack()
 
 
 
